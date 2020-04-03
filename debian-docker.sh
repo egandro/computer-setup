7a2b42e43e30a4b2d883f63798799ebff504b340
egandro <egandro@gmail.com>
2020-04-03 15:12:38 +0200

    Update debian-docker.sh

--- debian-docker.sh
+++ debian-docker.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+DOCKER_COMPOSE_VERSION=1.26.0-rc3
+DOCKER_MACHINE_VERSION=v0.16.2
+HETZNER_MACHINE_DRIVER_VERSION=2.1.0
 apt-get remove docker docker-engine docker.io containerd runc
 apt-get update
 apt-get install -y \
@@ -15,14 +18,14 @@ add-apt-repository \
    stable"
 apt-get update
 apt-get install docker-ce docker-ce-cli containerd.io -y
-curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-base=https://github.com/docker/machine/releases/download/v0.16.0
+curl -L "https://github.com/docker/compose/releases/download/$DOCKER_COMPOSE_VERSION/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+base=https://github.com/docker/machine/releases/download/$DOCKER_MACHINE_VERSION
 curl -L $base/docker-machine-$(uname -s)-$(uname -m) >/tmp/docker-machine
 install /tmp/docker-machine /usr/local/bin/docker-machine
 chmod +x /usr/local/bin/docker-machine
 rm /tmp/docker-machine
-wget https://github.com/JonasProgrammer/docker-machine-driver-hetzner/releases/download/1.2.2/docker-machine-driver-hetzner_1.2.2_linux_amd64.tar.gz
-tar -xvf docker-machine-driver-hetzner_1.2.2_linux_amd64.tar.gz
+wget https://github.com/JonasProgrammer/docker-machine-driver-hetzner/releases/download/${HETZNER_MACHINE_DRIVER_VERSION}/docker-machine-driver-hetzner_${HETZNER_MACHINE_DRIVER_VERSION}_linux_amd64.tar.gz
+tar -xvf docker-machine-driver-hetzner_${HETZNER_MACHINE_DRIVER_VERSION}_linux_amd64.tar.gz
 chmod +x docker-machine-driver-hetzner
 mv docker-machine-driver-hetzner /usr/local/bin/
-rm docker-machine-driver-hetzner_1.2.2_linux_amd64.tar.gz
+rm docker-machine-driver-hetzner_${HETZNER_MACHINE_DRIVER_VERSION}_linux_amd64.tar.gz
