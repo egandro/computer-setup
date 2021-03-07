4b536d4c5635f06af7ce183c2da979ed7876b619
egandro <egandro@gmail.com>
2021-03-07 13:21:02 +0100

    Update debian-install.sh

--- debian-install.sh
+++ debian-install.sh
@@ -23,7 +23,7 @@ cd vbox
 chmod 755 VBoxLinuxAdditions.run
 ./VBoxLinuxAdditions.run
 cd ..
-rm -f VBoxLinuxAdditions.run vbox
+rm -rf VBoxGuestAdditions_${VBOX_VERSION}.iso vbox
 
 
 ###################################################################
@@ -41,6 +41,7 @@ apt-get install -y nodejs
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
 sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm packages.microsoft.gpg
 
 apt install apt-transport-https
 apt update
