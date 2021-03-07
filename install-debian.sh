e8f0bd9cd6052147f70d330b8fb8a2397b4918cd
egandro <egandro@gmail.com>
2021-03-07 13:05:35 +0100

    Create install-debian.sh

---
+++ install-debian.sh
@@ -0,0 +1,116 @@
+#!/bin/bash
+set -x
+
+DEBIAN_USER=harald
+VBOX_VERSION=6.1.18
+
+###################################################################
+# vbox driver
+###################################################################
+
+apt update
+apt install -y wget curl gpg build-essential dkms linux-headers-$(uname -r)
+
+apt install -y  p7zip-full
+
+wget -c -t0 https://download.virtualbox.org/virtualbox/${VBOX_VERSION}/VBoxGuestAdditions_${VBOX_VERSION}.iso
+
+rm -rf vbox
+mkdir -p vbox
+cd vbox
+7z x ../VBoxGuestAdditions_${VBOX_VERSION}.iso
+chmod 755 VBoxLinuxAdditions.run
+./VBoxLinuxAdditions.run
+cd ..
+rm -f VBoxLinuxAdditions.run
+
+
+###################################################################
+# nodejs
+###################################################################
+
+curl -fsSL https://deb.nodesource.com/setup_current.x | bash -
+apt-get install -y nodejs
+
+
+###################################################################
+# visual studio code
+###################################################################
+
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+
+apt install apt-transport-https
+apt update
+apt install code 
+
+###################################################################
+# Chrome
+###################################################################
+
+wget -t0 -c  https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+apt install -y ./google-chrome-stable_current_amd64.deb
+apt --fix-broken install -y
+rm -f ./google-chrome-stable_current_amd64.deb
+
+
+###################################################################
+# Docker
+###################################################################
+
+apt-get update
+apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg
+
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
+  $(lsb_release -cs) stable" |  tee /etc/apt/sources.list.d/docker.list > /dev/null
+  
+apt-get update
+apt-get install -y docker-ce docker-ce-cli containerd.io
+
+usermod -a -G docker ${DEBIAN_USER}
+
+###################################################################
+# Tools
+###################################################################
+
+# Install dig tool for DNS querying
+apt install -y dnsutils
+
+# Install our version control systems
+apt install -y git subversion
+
+# Install ifconfig
+apt install -y net-tools
+
+# Install imc
+apt install -y mc
+
+# Install imc
+apt install -y screen
+
+
+###################################################################
+# Vim
+###################################################################
+
+apt install -y vim
+
+# Set vim as default editor
+su - ${DEBIAN_USER} /bin/bash -c "echo 'export EDITOR=vim' >> /home/${DEBIAN_USER}/.bashrc"
+su - ${DEBIAN_USER} /bin/bash -c "echo 3 | update-alternatives --config editor"
+
+
+# fix mouse
+
+# (FUCK! debian!)
+echo "if has('mouse')" >> /usr/share/vim/vim81/defaults.vim
+echo "   set mouse=r" >> /usr/share/vim/vim81/defaults.vim
+echo "endif" >> /usr/share/vim/vim81/defaults.vim
