744e9cd6fdfd84b4ce55ac5072a2d6cc50cecb83
egandro <egandro@gmail.com>
2021-04-01 13:57:25 +0200

    Update ssh-create-keys.sh

--- ssh-create-keys.sh
+++ ssh-create-keys.sh
@@ -2,6 +2,6 @@
 
 sudo apt-get install putty-tools || echo ""
 ssh-keygen
-puttygen ~/.ssh/id_rsa -o ~/.ssh/id_rsa.ppk
+puttygen ~/.ssh/id_rsa.pub -o ~/.ssh/id_rsa.ppk
 
 echo ssh-copy-id -i ~/.ssh/id_rsa ${USER}@host
