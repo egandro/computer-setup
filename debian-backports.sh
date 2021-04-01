66951a48e90197eb07e0c82d97978b1f8f7240da
egandro <egandro@gmail.com>
2021-04-01 23:47:09 +0200

    Update debian-backports.sh

--- debian-backports.sh
+++ debian-backports.sh
@@ -6,3 +6,7 @@ apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 04EE7237B7D453EC
 rm -f /etc/apt/sources.list.d/buster-backports.list
 echo "deb http://deb.debian.org/debian buster-backports main" | sudo tee -a /etc/apt/sources.list.d/buster-backports.list
 
+apt-get update
+
+echo apt-cache -t buster-backports search foo
+echo apt-get -t buster-backports install foo
