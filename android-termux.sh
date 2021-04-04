82bdfca3e1ee5d672c6e71fd4d50ca981e417250
egandro <egandro@gmail.com>
2021-04-04 02:18:47 +0200

    Update android-termux.sh

--- android-termux.sh
+++ android-termux.sh
@@ -1,1 +1,17 @@
-
+pkg update -y
+pkg upgrade -y
+termux-setup-storage
+pkg install -y vim
+pkg install -y screen
+pkg install -y mc
+pkg install -y make
+pkg install -y clang
+pkg install -y python
+pkg install -y nodejs
+pkg install -y git
+pkg install -y wget
+pkg install -y ffmpeg
+curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /data/data/com.termux/f
+iles/usr/bin/youtube-dl
+chmod a+rx /data/data/com.termux/files/usr/bin/youtube-dl
+apt autoremove -y
