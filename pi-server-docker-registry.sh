eb15ccfca7409edb9d9e37ef56a644fbdab1f8be
Harald Fielker <harald.fielker@gmail.com>
2021-04-08 22:24:21 +0200

    .

--- pi-server-docker-registry.sh
+++ pi-server-docker-registry.sh
@@ -14,33 +14,37 @@ RUN apt-get update \\
 ENTRYPOINT ["htpasswd", "-Bbn"]
 EOF
 
-mkdir -p ${GITLAB}/registry/auth
+REGISTRY_PASSWORD="$(< /dev/urandom tr -dc A-Z0-9 | head -c8)"
+
+mkdir -p ${GITLAB_HOME}/registry/auth
 
 docker build -f Dockerfile.htpasswd . -t htpasswd
-docker run --rm -ti htpasswd gitlab gitlab > ${GITLAB}/registry/auth/htpasswd
+docker run --rm -ti htpasswd gitlab ${REGISTRY_PASSWORD} > ${GITLAB_HOME}/registry/auth/htpasswd
+echo "${REGISTRY_PASSWORD}" > ${GITLAB_HOME}/registry/passwd
 rm -f Dockerfile.htpasswd
 docker image rm -f htpasswd || echo ""
 
 # no arm version
 #docker run \
 #  --entrypoint htpasswd \
-#  registry:2 -Bbn testuser testpassword > ${GITLAB}/registry/auth/htpasswd
+#  registry:2 -Bbn testuser testpassword > ${GITLAB_HOME}/registry/auth/htpasswd
 
-mkdir -p ${GITLAB}/registry/cert
+mkdir -p ${GITLAB_HOME}/registry/certs
 
-cp /etc/ssl/certs/wildcard.my.localnet.crt ${GITLAB}/registry/cert/server.crt
-cp /etc/ssl/private/wildcard.my.localnet.key ${GITLAB}/registry/cert/server.key
+cp /etc/ssl/certs/wildcard.my.localnet.crt ${GITLAB_HOME}/registry/certs/server.crt
+cp /etc/ssl/private/wildcard.my.localnet.key ${GITLAB_HOME}/registry/certs/server.key
+chmod 600 ${GITLAB_HOME}/registry/certs/server.key
 
 docker rm -f registry || echo ""
 docker run -d \
   --restart=always \
   --name registry \
-  -v ${GITLAB}/registry/registry:/var/lib/registry \
-  -v ${GITLAB}/registry/auth:/auth \
+  -v ${GITLAB_HOME}/registry/registry:/var/lib/registry \
+  -v ${GITLAB_HOME}/registry/auth:/auth \
   -e "REGISTRY_AUTH=htpasswd" \
   -e "REGISTRY_AUTH_HTPASSWD_REALM=Registry Realm" \
   -e REGISTRY_AUTH_HTPASSWD_PATH=/auth/htpasswd \
-  -v ${GITLAB}/registry/certs:/certs \
+  -v ${GITLAB_HOME}/registry/certs:/certs \
   -e REGISTRY_HTTP_ADDR=0.0.0.0:5000 \
   -e REGISTRY_HTTP_TLS_CERTIFICATE=/certs/server.crt \
   -e REGISTRY_HTTP_TLS_KEY=/certs/server.key \
