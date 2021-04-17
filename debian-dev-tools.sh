2fec6f07fbbec99fd7459eb86c7959b5bc71a77d
egandro <egandro@gmail.com>
2021-04-17 14:47:05 +0200

    Update debian-dev-tools.sh

--- debian-dev-tools.sh
+++ debian-dev-tools.sh
@@ -43,6 +43,15 @@ echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/
 sudo apt-get update
 sudo apt-get install -y kubectl
 
+#
+# helm
+#
+curl https://baltocdn.com/helm/signing.asc | sudo apt-key add -
+sudo apt-get install apt-transport-https --yes
+echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+sudo apt-get update
+sudo apt-get install -y helm
+
 
 #
 # kind (not Pi 32bit)
