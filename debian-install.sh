27c7cf734b780558e2e3ee84505ee8cdfbd67570
egandro <egandro@gmail.com>
2021-08-28 20:31:10 +0200

    Update debian-install.sh

--- debian-install.sh
+++ debian-install.sh
@@ -71,14 +71,17 @@ apt install yarn
 # visual studio code
 ###################################################################
 
-wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
-sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
-rm packages.microsoft.gpg
+if type Xorg 2>/dev/null; then
+  # we have an UI installed
+  wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+  install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+  sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+  rm packages.microsoft.gpg
 
-apt install -y apt-transport-https
-apt update
-apt install -y code 
+  apt install -y apt-transport-https
+  apt update
+  apt install -y code 
+fi
 
 ###################################################################
 # Chrome
@@ -127,7 +130,7 @@ then
 fi
 
 ###################################################################
-# Tools
+# CLI Tools
 ###################################################################
 
 # Install dig tool for DNS querying
