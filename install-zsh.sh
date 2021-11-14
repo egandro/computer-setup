24b45e7d8c61ff4c2ee3c701be623b1a7337e58f
egandro <egandro@gmail.com>
2021-11-14 15:50:08 +0100

    Create install-zsh.sh

---
+++ install-zsh.sh
@@ -0,0 +1,16 @@
+sudo apt-get install -y zsh
+chsh /usr/bin/zsh
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+cat << EOF > .zprofile
+emulate sh
+. ~/.profile
+emulate zsh
+EOF
+
+cat << EOF > .zshrc
+#https://lanbugs.de/howtos/linux-tipps-tricks/tmux-die-screen-alternative/#
+if [[ -z "$TMUX" ]] && [ "$SSH_CONNECTION" != "" ]; then
+        tmux attach-session -t ssh_tmux || tmux new-session -s ssh_tmux
+fi
+EOF
