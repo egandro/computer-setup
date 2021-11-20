cf3660d8b150f1dfd4e3444d05907dcbeb87dad7
egandro <egandro@gmail.com>
2021-11-20 17:25:48 +0100

    Update zram-install.sh

--- zram-install.sh
+++ zram-install.sh
@@ -1,3 +1,6 @@
+# https://ikarus.sg/using-zram-to-get-more-out-of-your-raspberry-pi/
+
 sudo apt-get install zram-tools
 sudo /bin/bash -c 'echo "PERCENT=50" >> /etc/default/zramswap'
 sudo systemctl restart zramswap.service
+
