f3674b8d239bb78dedc5b7227f6f8e5320bbdf9f
egandro <egandro@gmail.com>
2021-11-29 12:16:32 +0100

    Update debian-install.sh

--- debian-install.sh
+++ debian-install.sh
@@ -218,8 +218,9 @@ fi
 
 if [ ! -z "$IS_RASPBERRY" ]
 then
-  sed -i '$ s/$/ cgroup_memory=1 swapaccount=1 cgroup_enable=memory dwc_otg.lpm_enable=0/' /boot/cmdline.txt
-  yes | rpi-update
+  #sed -i '$ s/$/ cgroup_memory=1 swapaccount=1 cgroup_enable=memory dwc_otg.lpm_enable=0/' /boot/cmdline.txt
+  #yes | rpi-update
+  sed -i '$ s/$/ cgroup_enable=cpuset cgroup_enable=memory cgroup_memory=1 swapaccount=1/' /boot/cmdline.txt
 fi
 
 
