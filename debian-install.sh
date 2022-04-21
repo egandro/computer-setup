f7061c6f83609bd375432cf7d8808951af85ec03
egandro <egandro@gmail.com>
2022-04-21 15:53:04 +0200

    Update debian-install.sh

--- debian-install.sh
+++ debian-install.sh
@@ -133,7 +133,7 @@ usermod -a -G docker ${DEBIAN_USER}
 
 if [ -z "$IS_RASPBERRY" ]
 then
-  curl -L "https://github.com/docker/compose/releases/download/1.28.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+  curl -L "https://github.com/docker/compose/releases/download/v2.2.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
   chmod +x /usr/local/bin/docker-compose
 #else
 #this takes for ever to install on pi.
