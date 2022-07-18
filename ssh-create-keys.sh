aa2125d71aa4703db58f160283ec66ad49bb947e
egandro <egandro@gmail.com>
2022-07-18 22:40:45 +0200

    Update ssh-create-keys.sh

--- ssh-create-keys.sh
+++ ssh-create-keys.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 PASS=""
-ssh-keygen -b 2048 -t rsa -f ~/.ssh/id_rsa -q -N "${PASS}"
+ssh-keygen -t ed25519 -f ~/.ssh/id_ed25519 -q -N "${PASS}"
 
 # for windows/putty
 sudo apt-get install putty-tools || echo ""
-puttygen ~/.ssh/id_rsa -o ~/.ssh/id_rsa.ppk
+puttygen ~/.ssh/id_ed25519 -o ~/.ssh/id_ed25519.ppk
 
 # for windows to this machine
-ssh-copy-id -i ~/.ssh/id_rsa ${USER}@localhost
+ssh-copy-id -i ~/.ssh/id_ed25519 ${USER}@localhost
 
 # distribute key
-echo ssh-copy-id -i ~/.ssh/id_rsa ${USER}@host
-echo scp -p ~/.ssh/id_rsa ${USER}@host:/home/${USER}/.ssh
-echo scp -p ~/.ssh/id_rsa.pub ${USER}@host:/home/${USER}/.ssh
+echo ssh-copy-id -i ~/.ssh/id_ed25519 ${USER}@host
+echo scp -p ~/.ssh/id_ed25519 ${USER}@host:/home/${USER}/.ssh
+echo scp -p ~/.ssh/id_ed25519.pub ${USER}@host:/home/${USER}/.ssh
 
 # show keys ~/.ssh/authorized_keys
 # egrep '^[^#]' ~/.ssh/authorized_keys | xargs -n1 -I% bash -c 'ssh-keygen -l -f /dev/stdin <<<"%"'
