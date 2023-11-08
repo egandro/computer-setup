04905aab2a07f6c084d7f18e722ff079713c8f6e
egandro <egandro@gmail.com>
2023-11-08 16:00:43 +0100

    Update debian-install.sh

--- debian-install.sh
+++ debian-install.sh
@@ -84,8 +84,15 @@ fi
 # nodejs
 ###################################################################
 
-curl -fsSL https://deb.nodesource.com/setup_current.x | bash -
-apt-get install -y nodejs
+apt-get update
+apt-get install -y ca-certificates curl gnupg
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+NODE_MAJOR=20
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+apt-get update
+apt-get install nodejs -y
+
 # pi has installed node - remove old stuff
 apt autoremove -y
 
@@ -265,9 +272,10 @@ apt-get install -y exfat-utils
 ###################################################################
 # kubernetes and helm
 ###################################################################
-apt-get install -y apt-transport-https
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | tee -a /etc/apt/sources.list.d/kubernetes.list
+apt-get update
+apt-get install -y apt-transport-https ca-certificates curl g
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 apt-get update
 apt-get install -y kubectl
 
