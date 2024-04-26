e96740670d27a8523d7fe12ed7424c1fa8b74a1f
egandro <egandro@gmail.com>
2024-04-26 18:52:55 +0200

    Update android-termux.sh

--- android-termux.sh
+++ android-termux.sh
@@ -2,12 +2,10 @@ pkg update -y
 pkg upgrade -y
 termux-setup-storage
 pkg install -y vim
-pkg install -y screen
+echo "set mouse=r" >> $HOME/.vimrc
+pkg install -y tmux
+echo "set -g mouse off" >> $HOME/.tmux.conf
 pkg install -y mc
-pkg install -y make
-pkg install -y clang
-pkg install -y python
-pkg install -y nodejs
 pkg install -y git
 pkg install -y wget
 pkg install -y openssh
@@ -16,3 +14,8 @@ pkg install -y ffmpeg
 curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /data/data/com.termux/files/usr/bin/youtube-dl
 chmod a+rx /data/data/com.termux/files/usr/bin/youtube-dl
 apt autoremove -y
+
+#pkg install -y nodejs
+#pkg install -y make
+#pkg install -y clang
+#pkg install -y python
