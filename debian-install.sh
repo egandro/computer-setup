3073a7a346c02f40d6af2bd4757afaa931ca558c
egandro <egandro@gmail.com>
2024-12-27 03:48:25 +0100

    Update debian-install.sh

--- debian-install.sh
+++ debian-install.sh
@@ -85,13 +85,21 @@ fi
 ###################################################################
 
 apt-get update
-apt-get install -y ca-certificates curl gnupg
-mkdir -p /etc/apt/keyrings
-curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
-NODE_MAJOR=20
-echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+#apt-get install -y ca-certificates curl gnupg
+#mkdir -p /etc/apt/keyrings
+#curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+#NODE_MAJOR=20
+#echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+#apt-get update
+#apt-get install nodejs -y
+
+apt-get install -y curl
+curl -fsSL https://deb.nodesource.com/setup_22.x -o nodesource_setup.sh
+bash ./&nodesource_setup.sh
+rm -f nodesource_setup.sh
 apt-get update
 apt-get install nodejs -y
+
 
 # pi has installed node - remove old stuff
 apt autoremove -y
