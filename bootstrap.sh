a3d0c1c2cedbe25e06f5eb959f77f37de74d154d
Harald Fielker <harald.fielker@gmail.com>
2026-01-01 17:40:44 +0100

    delete debian-playbook.yml on exit

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+trap "rm -f debian-playbook.yml" EXIT
+
 apt update -y && apt install -y ansible curl sudo
 curl -fsSL https://raw.githubusercontent.com/egandro/computer-setup/main/debian-playbook.yml -o debian-playbook.yml
 ansible-playbook debian-playbook.yml
