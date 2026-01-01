08bf5f35ff9cc32c00f487728d6c90edea6bc1a2
Harald Fielker <harald.fielker@gmail.com>
2026-01-01 17:26:02 +0100

    apt

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-apt-get update && apt-get install -y ansible curl sudo
+apt update -y && apt install -y ansible curl sudo
 curl -fsSL https://raw.githubusercontent.com/egandro/computer-setup/main/debian-playbook.yml -o debian-playbook.yml
 ansible-playbook debian-playbook.yml
